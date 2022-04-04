9406d9616f71febb697ae70cfca926b532e9fd54
Wolfgang Scheer <31689731+wope4455@users.noreply.github.com>
2022-04-04 09:08:16 +0200

    Delete comsapbpBusinessPartners-1.0.1 directory

--- comsapbpBusinessPartners-1.0.1/Component-dbg.js
+++
@@ -1,9 +0,0 @@
-sap.ui.define(['sap/fe/core/AppComponent'], function(AppComponent) {
-    'use strict';
-
-    return AppComponent.extend('com.sap.bp.BusinessPartners.Component', {
-        metadata: {
-            manifest: 'json'
-        }
-    });
-});
--- comsapbpBusinessPartners-1.0.1/Component.js
+++
@@ -1,1 +0,0 @@
-sap.ui.define(["sap/fe/core/AppComponent"],function(e){"use strict";return e.extend("com.sap.bp.BusinessPartners.Component",{metadata:{manifest:"json"}})});
