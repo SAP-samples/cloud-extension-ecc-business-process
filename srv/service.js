1558ce07b34debc25fba21b907c99e9355d5dfe3
I532710 <syed.ejazuddin@sap.com>
2022-04-05 21:57:21 +0530

    s4addon-cflp

--- srv/service.js
+++ srv/service.js
@@ -4,89 +4,72 @@ module.exports = async srv => {
   const messaging = await cds.connect.to('messaging')
   const namespace = messaging.options.credentials && messaging.options.credentials.namespace
   const {postcodeValidator} = require('postcode-validator');
-  const {BusinessPartner: sdkBusinessPartner, BusinessPartnerAddress: sdkBusinessPartnerAddress}  = require('@sap/cloud-sdk-vdm-business-partner-service');
+  const log = cds.log("sales-service");
   
-  srv.on("READ", BusinessPartnerAddress, req => bupaSrv.tx(req).run(req.query))
-  srv.on("READ", BusinessPartner, req => bupaSrv.tx(req).run(req.query))
+  srv.on("READ", BusinessPartnerAddress, req => bupaSrv.run(req.query))
+  srv.on("READ", BusinessPartner, req => bupaSrv.run(req.query))
 
   messaging.on("refappscf/ecc/123/BO/BusinessPartner/Created", async msg => {
-    console.log("<< event caught", msg.event);
+    log.info("<< Create event caught", msg.data);
     let BUSINESSPARTNER = "";
-    BUSINESSPARTNER = parseInt(msg.data.BUT000[0].PARTNER);
-    console.log("<<< Received Created Business Partner Id " + BUSINESSPARTNER);
-    let BusinessPartnerName = "";
-    if(msg.data.BUT000[0].NAME1_TEXT == ""){
-      BusinessPartnerName = msg.data.BUT000[0].NAME_FIRST + ' ' + msg.data.BUT000[0].NAME_LAST;
-    }
-    else{
-      BusinessPartnerName = msg.data.BUT000[0].NAME1_TEXT;  
-    }
+    BUSINESSPARTNER = msg.data.BUT000[0].PARTNER;
+    let BusinessPartnerName = msg.data.BUT000[0].NAME1_TEXT == "" ? msg.data.BUT000[0].NAME_FIRST + ' ' + msg.data.BUT000[0].NAME_LAST : msg.data.BUT000[0].NAME1_TEXT;
     const result = await cds.run(INSERT.into(Notifications).entries({businessPartnerId:BUSINESSPARTNER, verificationStatus_code:'N', businessPartnerName:BusinessPartnerName}));        
+    let adrc = msg.data.BUT000[0]?.ADRC[0];
+    console.log("<<adrc",adrc)
     let address = {
-            addressId: parseInt(msg.data.BUT000[0].ADRC[0].BUT020[0].ADDRNUMBER),
-            country: msg.data.BUT000[0].ADRC[0].COUNTRY,
-            cityName: msg.data.BUT000[0].ADRC[0].CITY1,
-            streetName: msg.data.BUT000[0].ADRC[0].STREET,
-            postalCode: msg.data.BUT000[0].ADRC[0].POST_CODE1,
+            addressId: parseInt(adrc?.BUT020[0].ADDRNUMBER),
+            country: adrc?.COUNTRY,
+            cityName: adrc?.CITY1,
+            streetName: adrc?.STREET,
+            postalCode: adrc?.POST_CODE1,
             businessPartnerId: BUSINESSPARTNER
        }
       if(address) {  
        const notificationObj = await cds.run(SELECT.one(Notifications).columns("ID").where({businessPartnerId: BUSINESSPARTNER}));
        address.notifications_ID=notificationObj.ID
        const res = await cds.run(INSERT.into(Addresses).entries(address));
-       console.log("Address inserted");
+       log.info("Address inserted");
        }  
   });
 
   messaging.on("refappscf/ecc/123/BO/BusinessPartner/Changed", async msg => {
-
     let BUSINESSPARTNER = "";
-    let fullName = "";
-    BUSINESSPARTNER = parseInt(msg.data.objectId);
-    BUSINESSPARTNER = (+BUSINESSPARTNER).toString()
-    console.log("<<< Received Changed Business Partner Id " + BUSINESSPARTNER);    
-    const bpIsAlive = await cds.tx(msg).run(SELECT.one(Notifications , (n) => n.verificationStatus_code).where({businessPartnerId: BUSINESSPARTNER}));
-    const notif = await cds.run(SELECT.from(Notifications).where({businessPartnerId: BUSINESSPARTNER}))
-    let name = notif[0].businessPartnerName
-    var name_split = name.split(" ");
-    let firstname = name_split[0];
-    let lastname = name_split[1];
+    BUSINESSPARTNER = msg.data.objectId;
+    log.info("<<< Received Changed Business Partner Id " + BUSINESSPARTNER);    
+    const bpIsAlive = await cds.run(SELECT.one(Notifications , (n) => n.verificationStatus_code).where({businessPartnerId: BUSINESSPARTNER}));
+    if(bpIsAlive) {
+        const notif = await cds.run(SELECT.from(Notifications).where({businessPartnerId: BUSINESSPARTNER}))
+        let name = notif[0].businessPartnerName
+        var name_split = name.split(" ");
+        let firstname = name_split[0];        
+        let lastname = name_split[1];
 
-    if(bpIsAlive && msg.data.NAME_LAST) {
-        console.log("<<< Received last name", msg.data.NAME_LAST)
-        lastname = msg.data.NAME_LAST;
-        fullName = firstname + " " + lastname;
-        const bpMarkVerified= await cds.tx(msg).run(UPDATE(Notifications).where({businessPartnerId: BUSINESSPARTNER}).set({businessPartnerName:fullName}));
-        console.log("<< BP Last Name Updated >>")  
-    }  
+    if (msg.data.NAME_FIRST || msg.data.NAME_LAST) {
+      let fullName = msg.data.NAME_LAST ? (msg.data.NAME_FIRST ? (msg.data.NAME_FIRST + ' ' + msg.data.NAME_LAST) : (firstname + ' ' + msg.data.NAME_LAST)) : (msg.data.NAME_FIRST + ' ' + lastname);
+      log.info("<<<full name",fullName)
+      const bpMarkVerified = await cds.run(UPDATE(Notifications).where({businessPartnerId: BUSINESSPARTNER}).set({businessPartnerName:fullName}));
+     }
 
-    if(bpIsAlive && msg.data.NAME_FIRST) {
-        console.log("<<< Received first name", msg.data.NAME_FIRST)
-        firstname = msg.data.NAME_FIRST;
-        fullName = firstname + " " + lastname;
-        const bpMarkVerified= await cds.tx(msg).run(UPDATE(Notifications).where({businessPartnerId: BUSINESSPARTNER}).set({businessPartnerName:fullName}));
-        console.log("<< BP First Name Updated >>")  
-    }      
-
-
-    if(bpIsAlive && msg.data.XBLCK == "") {
+    if(msg.data.XBLCK == "") {
         bpIsAlive.verificationStatus_code = "V"
-        console.log("<< BP marked Verified >>")  
+        log.info("<< BP marked Verified >>")  
     }  
      
-    if(bpIsAlive && msg.data.XBLCK == "X") {
-        const bpMarkVerified= await cds.tx(msg).run(UPDATE(Notifications).where({businessPartnerId: BUSINESSPARTNER}).set({verificationStatus_code:"P"}));
-        console.log("<< BP marked Processed >>")  
+    if(msg.data.XBLCK == "X") {
+        const bpMarkVerified= await cds.run(UPDATE(Notifications).where({businessPartnerId: BUSINESSPARTNER}).set({verificationStatus_code:"P"}));
+        log.info("<< BP marked Processed >>")  
     }   
 
-    if(bpIsAlive && bpIsAlive.verificationStatus_code == "V") {
-        const bpMarkVerified= await cds.tx(msg).run(UPDATE(Notifications).where({businessPartnerId: BUSINESSPARTNER}).set({verificationStatus_code:"C"}));
-        console.log("<< BP marked Completed >>")  
-    }    
+    if(bpIsAlive.verificationStatus_code == "V") {
+        const bpMarkVerified= await cds.run(UPDATE(Notifications).where({businessPartnerId: BUSINESSPARTNER}).set({verificationStatus_code:"C"}));
+        log.info("<< BP marked Completed >>")  
+    } 
+   }     
   });
 
   srv.after("UPDATE", "Notifications", (data, req) => {
-    console.log("Notification update", data.businessPartnerId);
+    log.info("Notification update", data.businessPartnerId);
     if(data.verificationStatus_code === "V" || data.verificationStatus_code === "INV")
     emitEvent(data, req);
   });
@@ -98,7 +81,7 @@ module.exports = async srv => {
   });
 
   srv.before("PATCH", "Addresses", req => {
-    // To set whether address is Edited
+  // To set whether address is Edited
     req.data.isModified = true;
   });
 
@@ -111,7 +94,7 @@ module.exports = async srv => {
   });
 
   async function emitEvent(result, req){
-    const resultJoin =  await cds.tx(req).run(SELECT.one("my.businessPartnerValidation.Notifications as N").leftJoin("my.businessPartnerValidation.Addresses as A").on("N.businessPartnerId = A.businessPartnerId").where({"N.ID": result.ID}));
+    const resultJoin =  await cds.run(SELECT.one("my.businessPartnerValidation.Notifications as N").leftJoin("my.businessPartnerValidation.Addresses as A").on("N.businessPartnerId = A.businessPartnerId").where({"N.ID": result.ID}));
     const statusValues={"N":"NEW", "P":"PROCESS", "INV":"INVALID", "V":"VERIFIED"}
 
     if(resultJoin.isModified){
@@ -119,25 +102,15 @@ module.exports = async srv => {
         streetName: resultJoin.streetName,
         postalCode: resultJoin.postalCode
       }
-      let payloadBuilder = sdkBusinessPartnerAddress.builder().fromJson(payload);
-      payloadBuilder.businessPartner = resultJoin.businessPartnerId;
-      payloadBuilder.addressId = resultJoin.addressId
-
-      let res = await sdkBusinessPartnerAddress.requestBuilder().update(payloadBuilder).withCustomServicePath("/").execute({
-        destinationName: bupaSrv.destination
-      });
-      console.log("address update to S4", res);
+      let res = await bupaSrv.run(UPDATE(BusinessPartnerAddress).set(payload).where({businessPartnerId:resultJoin.businessPartnerId, addressId:resultJoin.addressId}));
+      log.info("address update to S4", res);
     }
 
     let payload = {
       "searchTerm1": statusValues[resultJoin.verificationStatus_code],
       "businessPartnerIsBlocked": (resultJoin.verificationStatus_code == "V")?false:true
     }
-    let payloadBuilder = sdkBusinessPartner.builder().fromJson(payload);
-    payloadBuilder.businessPartner = resultJoin.businessPartnerId;
-    let res = await sdkBusinessPartner.requestBuilder().update(payloadBuilder).withCustomServicePath("/").execute({
-      destinationName: bupaSrv.destination
-    });
-    console.log("Search Term update", res);
+    let res =  await bupaSrv.run(UPDATE(BusinessPartner, resultJoin.businessPartnerId).with(payload)); 
+    log.info("Search Term update", res);
   } 
 }
--- srv/ui-annotations.cds
+++ srv/ui-annotations.cds
@@ -1,4 +1,3 @@
-
  using service.businessPartnerValidation.SalesService as my from './service';
 
 annotate my.Addresses with @(
@@ -11,11 +10,11 @@ annotate my.Addresses with @(
         SelectionFields: [ identifier, title, availability, price],
         LineItem: [
             {$Type: 'UI.DataField', Value: addressId},
-            {$Type: 'UI.DataField', Value: businessPartnerId},
-            {$Type: 'UI.DataField', Value: streetName},
+            {$Type: 'UI.DataField', Value: businessPartnerId}, 
+            {$Type: 'UI.DataField', Value: streetName,![@HTML5.CssDefaults] : {width : '100%'}},
             {$Type: 'UI.DataField', Value: cityName},
             {$Type: 'UI.DataField', Value: country},
-            {$Type: 'UI.DataField', Value: postalCode}
+            {$Type: 'UI.DataField', Value: postalCode},
         ],
         HeaderFacets: [
             {$Type: 'UI.ReferenceFacet', ID: 'BP', Target: '@UI.DataPoint#BP'},
@@ -51,9 +50,9 @@ annotate my.Notifications with @(
         },
          SelectionFields: [ businessPartnerId, businessPartnerName,verificationStatus_code],
         LineItem: [
-          {$Type: 'UI.DataField', Value: businessPartnerId},
-          {$Type: 'UI.DataField', Value: businessPartnerName},
-          {$Type: 'UI.DataField', Value: verificationStatus.value, Criticality: verificationStatus.criticality}
+          {$Type: 'UI.DataField', Value: businessPartnerId, ![@HTML5.CssDefaults] : {width : '33.33%'}},
+          {$Type: 'UI.DataField', Value: businessPartnerName, ![@HTML5.CssDefaults] : {width : '33.33%'}},
+          {$Type: 'UI.DataField', Value: verificationStatus.value, Criticality: verificationStatus.criticality,![@HTML5.CssDefaults] : {width : '33.33%'}}
           
         ],
          HeaderFacets: [
@@ -101,23 +100,3 @@ annotate my.StatusValues with {
     } @title :  'Code';
     value @title: 'Verification Status';
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
