4ad7fe096af5ff9fd1d4cb3955685fc8b30f9e63
i332371 <premnavin2000@gmail.com>
2023-07-24 10:54:35 +0530

    cds7 upgrade

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const customIndexJS = require ('./customIndex.js')
 
 module.exports = cds.server
 
-const cds_swagger = require ('cds-swagger-ui-express')
-cds.on ('bootstrap', app => app.use (cds_swagger()) )
+
 
 cds.on("bootstrap", app => app.use(proxy()) );
 
 cds.on('bootstrap', app => {
   app.get ('/', (_,res) => res.send (customIndexJS.html)) 
 })
+
+const cds_swagger = require ('cds-swagger-ui-express')
+cds.on ('bootstrap', app => app.use (cds_swagger()) )
