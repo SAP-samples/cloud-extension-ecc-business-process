f988968f8c41f2d31ed99a75d5892ed963118546
i332371 <premnavin2000@gmail.com>
2024-01-09 18:35:53 +0530

    v2 adapter migration * Community v2 proxy adapter

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const cds = require ('@sap/cds')
-const proxy = require("@sap/cds-odata-v2-adapter-proxy");
+// const proxy = require("@sap/cds-odata-v2-adapter-proxy");
 const customIndexJS = require ('./customIndex.js')
 
 module.exports = cds.server
 
 
 
-cds.on("bootstrap", app => app.use(proxy()) );
+// cds.on("bootstrap", app => app.use(proxy()) );
 
 cds.on('bootstrap', app => {
   app.get ('/', (_,res) => res.send (customIndexJS.html)) 
